2e63a6e8ffff141717c72ebfd5e0e5083ba2470c
Michael Kret <dirtysculptor@gmail.com>
2021-06-13 07:45:38 +0300

    working on mobile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,27 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        barlow: ["Barlow", "sans-serif"],
+        fraunces: ["Fraunces", "serif"],
+      },
+      colors: {
+        brand: {
+          "soft-red": "hsl(7, 99%, 70%)",
+          yellow: "hsl(51, 100%, 49%)",
+          "dark-cyan": "hsl(167, 40%, 24%)",
+          "dark-blue": "hsl(198, 62%, 26%)",
+          cyan: "hsl(168, 34%, 41%)",
+        },
+      },
+      backgroundImage: (theme) => ({
+        header: "url('/images/mobile/image-header.jpg')",
+      }),
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
